9d1b79ac12ec9f281a0263747472deacbcf2dda1
khadijatarek <khadijaamer01@gmail.com>
2023-11-30 23:35:51 +0200

    some repository functions

--- VeseetaProject.Core/Repositories/IBaseRepository.cs
+++ VeseetaProject.Core/Repositories/IBaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,21 @@ namespace VeseetaProject.Core.Repositories
 {
     public interface IBaseRepository<T> where T : class
     {
-        Task <IEnumerable<T>> GetAll();
+        Task <IEnumerable<T>> GetAll(Expression<Func<T, bool>>? criteria,  int? pageNo, int? pageSize,string[]? includes= null);
+
+        Task<T> GetById(int id);
+        Task<T> GetById(string id);
+
+        Task<T> Find(Expression<Func<T, bool>> criteria);
+
+        Task<int> Count(Expression<Func<T, bool>>? criteria);
+        
+        Task<T> Add(T entity);
+        Task<IEnumerable<T>> AddRange(IEnumerable<T> entities);
+
+        //T Update(T entity);
+
+        //void DeleteById(int id);
+        //void DeleteById(string id);
     }
 }
--- VeseetaProject.Data/Repositories/BaseRepository.cs
+++ VeseetaProject.Data/Repositories/BaseRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using VeseetaProject.Core.Repositories;
@@ -20,5 +21,101 @@ namespace VeseetaProject.Data.Repositories
         {
             return await _context.Set<T>().ToListAsync();
         }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T,bool>> criteria, string[]? includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return await query.Where(criteria).ToListAsync();
+
+        }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? criteria, int? pageNo, int? pageSize, string[]? includes = null)
+        {
+            int? startIndex = (pageNo - 1) * pageSize;
+            IQueryable<T> query = _context.Set<T>();
+            
+            if (criteria != null)
+            {
+                query = query.Where(criteria);
+            }
+            
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            if (startIndex.HasValue)
+            {
+                query = query.Skip(startIndex.Value);
+            }
+
+            if (pageSize.HasValue)
+            {
+                query = query.Take(pageSize.Value);
+            }
+
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<T> Find(Expression<Func<T, bool>> criteria)
+        {
+            var result = await _context.Set<T>().SingleOrDefaultAsync(criteria);
+            if (result != null)
+                return result;
+                
+            return null;
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            var result = await _context.Set<T>().FindAsync(id);
+            if (result != null)
+                return result;
+
+            return null;
+        }
+        public async Task<T> GetById(string id)
+        {
+            var result = await _context.Set<T>().FindAsync(id);
+            if (result != null)
+                return result;
+
+            return null;
+        }
+
+        public async Task<int> Count(Expression<Func<T, bool>>? criteria)
+        {
+            if (criteria == null)
+            {
+                return await _context.Set<T>().CountAsync();
+            }
+            return await _context.Set<T>().CountAsync(criteria);
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+        public async Task<IEnumerable<T>> AddRange(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+            return entities;
+        }
+
+
+     
     }
 }
